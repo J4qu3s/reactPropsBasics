dce71afbbadf8822343f22b8f60dcefae327d5b9
J4qu3s <jarkko.hoyhtya@gmail.com>
2021-10-25 23:13:55 +0300

    ComponentsFinal
    Exercise with react props

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
+const fakerDate = faker.date.past();
+console.log(fakerDate);
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img src="" alt="avatar" />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">
-                            Today : 6.21 AM
-                        </span>
-                    </div>
-                    <div className="text">
-                        <p>
-                            What a nice blog post!
-                        </p>
-                    </div>
-                </div>
-            </div>          
+
+            <ApprovalCard>
+                <CommentDetail
+                    image={faker.image.avatar()}
+                    author={faker.name.firstName()}
+                    time="11.10 AM"
+                    content={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    image={faker.image.avatar()}
+                    author={faker.name.firstName()}
+                    time="12.53 AM"
+                    content={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail
+                    image={faker.image.avatar()}
+                    author={faker.name.firstName()}
+                    time="1.23 pM"
+                    content={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+
+            
+
+            
+
         </div>
     )
 };
